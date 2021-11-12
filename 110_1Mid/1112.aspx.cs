216b533014f0aefe69d0459b74306aafe295e354
宗凱 鄭 <108111106@mail.aeust.edu.tw>
2021-11-12 09:23:08 +0800

    sb

---
+++ 110_1Mid/1112.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _110_1Mid
+{
+    public partial class _1112 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
